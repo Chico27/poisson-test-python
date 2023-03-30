24cd1936e27d4631a32635231233a0fd99096edc
Chico27 <masamasa.987654321@gmail.com>
2023-03-31 00:05:40 +0900

    blue archive pickup poisson

--- blue_archive_pickup_poisson.py
+++ blue_archive_pickup_poisson.py
@@ -2,3 +2,8 @@ from scipy.stats import poisson
 l = 200*0.007
 for i in range(11):
     print('ピックアップが' + str(i) + '回出る確率は' + str(poisson.pmf(i, l) * 100) + '%')
+
+sum = 0
+for i in range(10):
+    sum += poisson.pmf(i, l) * 100
+    print('ピックアップが' + str(i+1) + '回以上出る確率は' + str(100 - sum) + '%')
